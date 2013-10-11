86645734911120752d04290380291fb840f0203c
jamescarr-mango <jcarr@mango-solutions.com>
2013-10-11 09:15:43 +0100

    DDMORE-187 added missing API comments

--- main/WP2/eu.ddmore.converter.toolbox/src/main/java/eu/ddmore/convertertoolbox/api/java/Converter.java
+++ main/WP2/eu.ddmore.converter.toolbox/src/main/java/eu/ddmore/convertertoolbox/api/java/Converter.java
@@ -12,11 +12,27 @@ import eu.ddmore.convertertoolbox.response.ConversionReport;
  */
 public interface Converter {
 
+    /**
+     * Convert the source file and put the output file(s) in the output directory. 
+     * @return a conversion report containing the details of the requested conversion 
+     */
     ConversionReport convert(File src, File outputDirectory);
 
+    /**
+     * Convert each of the source files and put the output file(s) in the output directory. 
+     * @return a conversion report containing the details of the requested conversion 
+     */
     ConversionReport convert(File[] src, File outputDirectory);
 
+    /**
+     * Convert the source file and put the output file(s) in the output directory. 
+     * @param listener a callback hook, if specified the listener will be notified by the converter when certain states are reached  
+     */
     void convert(File src, File outputDirectory, ConversionListener listener);
 
+    /**
+     * Convert each of the source files and put the output file(s) in the output directory. 
+     * @param listener a callback hook, if specified the listener will be notified by the converter when certain states are reached
+     */
     void convert(File[] src, File outputDirectory, ConversionListener listener);
 }
--- main/WP2/eu.ddmore.converter.toolbox/src/main/java/eu/ddmore/convertertoolbox/api/java/ConverterManager.java
+++ main/WP2/eu.ddmore.converter.toolbox/src/main/java/eu/ddmore/convertertoolbox/api/java/ConverterManager.java
@@ -13,9 +13,24 @@ import eu.ddmore.convertertoolbox.domain.Version;
  */
 public interface ConverterManager {
 
+    /**
+     * Get the converter which is registered with the framework providing conversion from the specified input to output. 
+     * If there is more than one version of the converter registered the latest one (highest version) will be returned.
+     * @return a ready to use converter 
+     * @throws if there is no converter registered in the toolbox that can convert from input to output
+     */
     Converter getConverter(LanguageVersion source, LanguageVersion target);
 
+    /**
+     * Get the converter which is registered with the framework providing conversion from the specified input to output and the converter version matches that specified.
+     * @return a ready to use converter
+     * @throws if there is no converter registered in the toolbox that can convert from input to output and is of the specified version
+     */
     Converter getConverter(LanguageVersion source, LanguageVersion target, Version converterVersion);
 
+    /**
+     * Get the capabilities of the toolbox, this could change over time (runtime mutable)
+     * @return the list of input and output language/version pairs which are supported by the toolbox at the time of calling this method 
+     */
     Map<LanguageVersion, LanguageVersion> getCapabilities();
 }
