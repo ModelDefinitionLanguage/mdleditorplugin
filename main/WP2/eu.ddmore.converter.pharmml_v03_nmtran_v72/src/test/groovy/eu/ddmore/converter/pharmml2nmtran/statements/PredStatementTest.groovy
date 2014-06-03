d601de7c7b8ceab203631b0fbc038838984ac26b
Jonathan Chard <jchard@mango-solutions.com>
2014-06-03 13:24:46 +0100

    DDMORE-701 : Count etas correctly

--- main/WP2/eu.ddmore.converter.pharmml_v03_nmtran_v72/src/test/groovy/eu/ddmore/converter/pharmml2nmtran/statements/PredStatementTest.groovy
+++ main/WP2/eu.ddmore.converter.pharmml_v03_nmtran_v72/src/test/groovy/eu/ddmore/converter/pharmml2nmtran/statements/PredStatementTest.groovy
@@ -11,12 +11,15 @@ import eu.ddmore.libpharmml.dom.PharmML
 
 import org.apache.commons.io.FileUtils;
 import static eu.ddmore.converter.pharmml2nmtran.MainTest.PATH
+import static eu.ddmore.converter.pharmml2nmtran.statements.NMTranFormatter.PREFIX
 
 public class PredStatementTest {
 
+	private final String prolactinModel = PATH + "Friberg2009Prolactin_v20140523v14_NONMEM.xml"
+	
 	@Test
 	public void testSimpleAssignedParameters() {
-        File src = new File(Thread.currentThread().getContextClassLoader().getResource(PATH +'Friberg2009Prolactin_v20140506v13_NONMEM.xml').getPath());
+        File src = new File(Thread.currentThread().getContextClassLoader().getResource(prolactinModel).getPath());
         def pmlAPI = PharmMlFactory.getInstance().createLibPharmML()
 		def is = FileUtils.openInputStream(src)
 		PharmML pmlDOM = pmlAPI.createDomFromResource(is).getDom()
@@ -30,12 +33,13 @@ public class PredStatementTest {
 		String pieceWiseAsNmtran = conversionContext.convert( PHS1.value.assign.equation.piecewise, "PHS1", conversionContext.simpleParameterToNmtran)
 			
 		// Think this is ok - no simple parameters in structural model, so we can't name the thetas
-		assertEquals("IF(NM_PAT.EQ.NM_HV)\n	NM_PHS1=THETA(9)\nELSE\nIF(NM_PAT.EQ.NM_PAT)\n	NM_PHS1=THETA(9)+THETA(8)\nENDIF\n", pieceWiseAsNmtran )
+		String expected = "IF(${PREFIX}PAT.EQ.${PREFIX}HV)\n	${PREFIX}PHS1=THETA(9)\nELSE\nIF(${PREFIX}PAT.EQ.${PREFIX}PAT)\n	${PREFIX}PHS1=THETA(9)+THETA(8)\nENDIF\n"
+		assertEquals(expected, pieceWiseAsNmtran )
  	}
 
 	@Test
 	public void testComplexAssignedParameters() {
-        File src = new File(Thread.currentThread().getContextClassLoader().getResource(PATH +'Friberg2009Prolactin_v20140506v13_NONMEM.xml').getPath());
+        File src = new File(Thread.currentThread().getContextClassLoader().getResource(prolactinModel).getPath());
         def pmlAPI = PharmMlFactory.getInstance().createLibPharmML()
 		def is = FileUtils.openInputStream(src)
 		PharmML pmlDOM = pmlAPI.createDomFromResource(is).getDom()
@@ -48,29 +52,28 @@ public class PredStatementTest {
 		
 		String pieceWiseAsNmtran = conversionContext.convert( POP_PRL0.value.assign.equation.piecewise, "POP_PRL0", conversionContext.simpleParameterToNmtran)
 			
-String expected = """IF(NM_PAT.EQ.NM_HV)
-	NM_POP_PRL0=THETA(1)
+String expected = """IF(${PREFIX}PAT.EQ.${PREFIX}HV)
+	${PREFIX}POP_PRL0=THETA(1)
 ELSE
-IF(((NM_PAT.EQ.NM_PAT).AND.(NM_SEX.EQ.NM_M)).AND.(NM_STUDY.NE.NM_STU101))
-	NM_POP_PRL0=THETA(2)
+IF(((${PREFIX}PAT.EQ.${PREFIX}PAT).AND.(${PREFIX}SEX.EQ.${PREFIX}M)).AND.(${PREFIX}STUDY.NE.${PREFIX}STU101))
+	${PREFIX}POP_PRL0=THETA(2)
 ELSE
-IF(((NM_PAT.EQ.NM_PAT).AND.(NM_SEX.EQ.NM_F)).AND.(NM_STUDY.NE.NM_STU101))
-	NM_POP_PRL0=THETA(3)
+IF(((${PREFIX}PAT.EQ.${PREFIX}PAT).AND.(${PREFIX}SEX.EQ.${PREFIX}F)).AND.(${PREFIX}STUDY.NE.${PREFIX}STU101))
+	${PREFIX}POP_PRL0=THETA(3)
 ELSE
-IF(((NM_PAT.EQ.NM_PAT).AND.(NM_SEX.EQ.NM_M)).AND.(NM_STUDY.EQ.NM_STU101))
-	NM_POP_PRL0=THETA(4)
+IF(((${PREFIX}PAT.EQ.${PREFIX}PAT).AND.(${PREFIX}SEX.EQ.${PREFIX}M)).AND.(${PREFIX}STUDY.EQ.${PREFIX}STU101))
+	${PREFIX}POP_PRL0=THETA(4)
 ELSE
-IF(((NM_PAT.EQ.NM_PAT).AND.(NM_SEX.EQ.NM_F)).AND.(NM_STUDY.EQ.NM_STU101))
-	NM_POP_PRL0=THETA(5)
+IF(((${PREFIX}PAT.EQ.${PREFIX}PAT).AND.(${PREFIX}SEX.EQ.${PREFIX}F)).AND.(${PREFIX}STUDY.EQ.${PREFIX}STU101))
+	${PREFIX}POP_PRL0=THETA(5)
 ENDIF
 """
-
 		assertEquals(expected, pieceWiseAsNmtran )
  	}
 
 	@Test
 	public void testEtaCount() {
-		File src = new File(Thread.currentThread().getContextClassLoader().getResource(PATH +'Friberg2009Prolactin_v20140506v13_NONMEM.xml').getPath());
+		File src = new File(Thread.currentThread().getContextClassLoader().getResource(prolactinModel).getPath());
 		def pmlAPI = PharmMlFactory.getInstance().createLibPharmML()
 		def is = FileUtils.openInputStream(src)
 		PharmML pmlDOM = pmlAPI.createDomFromResource(is).getDom()
@@ -86,49 +89,21 @@ ENDIF
 		
 		String[] lines = ind.split("\\n")
 		String nm_kout = lines[0].split("=")[1]
-		assertEquals(nm_kout, "NM_POP_KOUT*EXP(ETA(4))")
+		String expected = "${PREFIX}POP_KOUT*EXP(ETA(4))"
+		assertEquals(expected, nm_kout)
 		
 		String nm_prl0 = lines[1].split("=")[1]
-		assertEquals(nm_prl0, "NM_POP_PRL0*EXP(ETA(1))*EXP(ETA(3))")
+		expected = "${PREFIX}POP_PRL0*EXP(ETA(1))*EXP(ETA(3))"
+		assertEquals(expected, nm_prl0 )
 		
+		String amp1 = lines[2].split("=")[1]
+		expected = "${PREFIX}POP_AMP1*EXP(ETA(5))"
+		assertEquals(expected, amp1)
+		
+		String phs2 = lines[3].split("=")[1]
+		expected = "${PREFIX}THETA(11)+ETA(6)"
+		assertEquals(expected, phs2 )
+
 	}
 	
-	/**
-	 * 	NM_KOUT=NM_POP_KOUT*EXP(ETA(4))
-	NM_PRL0=NM_POP_PRL0*EXP(ETA(1))*EXP(ETA(3))
-	NM_AMP1=NM_POP_AMP1*EXP(ETA(5))
-	NM_PHS2=THETA(11)+ETA(5)
-	NM_KI=NM_POP_KI*EXP(ETA(2))
-	NM_D2=0.5
-	NM_F2=1.0
-	IF(NM_PAT.EQ.NM_HV)
-	NM_PHS1=THETA(9)
-ELSE
-IF(NM_PAT.EQ.NM_PAT)
-	NM_PHS1=THETA(9)+THETA(8)
-ENDIF
-
-	IF(NM_PAT.EQ.NM_HV)
-	NM_POP_PRL0=THETA(1)
-ELSE
-IF(IF(IF(NM_PAT.EQ.NM_PAT).AND.IF(NM_SEX.EQ.NM_M)).AND.IF(NM_STUDY.NE.NM_STU101))
-	NM_POP_PRL0=THETA(2)
-ELSE
-IF(IF(IF(NM_PAT.EQ.NM_PAT).AND.IF(NM_SEX.EQ.NM_F)).AND.IF(NM_STUDY.NE.NM_STU101))
-	NM_POP_PRL0=THETA(3)
-ELSE
-IF(IF(IF(NM_PAT.EQ.NM_PAT).AND.IF(NM_SEX.EQ.NM_M)).AND.IF(NM_STUDY.EQ.NM_STU101))
-	NM_POP_PRL0=THETA(4)
-ELSE
-IF(IF(IF(NM_PAT.EQ.NM_PAT).AND.IF(NM_SEX.EQ.NM_F)).AND.IF(NM_STUDY.EQ.NM_STU101))
-	NM_POP_PRL0=THETA(5)
-ENDIF
-
-	IF(NM_PAT.EQ.NM_HV)
-	NM_POP_PHS2=THETA(11)
-ELSE
-IF(NM_PAT.EQ.NM_PAT)
-	NM_POP_PHS2=THETA(11)+THETA(8)
-ENDIF
-	 */
 }
