871cd8847b18bcdf5f2cc926fe661400d72c1135
Matthew Wise <mwise@mango-solutions.com>
2014-06-16 14:58:57 +0100

    DDMORE-712: Move the models from out of the toolbox-int-test project: Move the models out of eu.ddmore.converter.test project and into eu.ddmore.testdata.models project; Bring in the testdata models dependency into that converter-test project and update tests so that they now pass.

--- main/WP2/eu.ddmore.converter.test/src/test/java/eu/ddmore/converter/test/MainIT.java
+++ main/WP2/eu.ddmore.converter.test/src/test/java/eu/ddmore/converter/test/MainIT.java
@@ -5,8 +5,11 @@ package eu.ddmore.converter.test;
 
 import static org.junit.Assert.assertEquals;
 
+import java.io.File;
 import java.io.IOException;
 
+import org.apache.commons.io.FileUtils;
+import org.junit.Before;
 import org.junit.Test;
 
 import eu.ddmore.convertertoolbox.api.exception.ConverterNotFoundException;
@@ -20,25 +23,35 @@ import eu.ddmore.convertertoolbox.cli.Main;
  */
 public class MainIT {
 
+    private final static String modelFilePath = "/eu/ddmore/testdata/models/mdl/warfarin_PK_PRED/warfarin_PK_PRED.mdl";
+    private final static String WORKING_DIRECTORY = "target/MainIT_Working_Dir/";
+
+    @Before
+    public void setUp() throws IOException {
+        FileUtils.deleteDirectory(new File(WORKING_DIRECTORY));
+        FileUtils.copyInputStreamToFile(
+            MainIT.class.getResourceAsStream(modelFilePath),
+            new File(WORKING_DIRECTORY, "warfarin_PK_PRED.mdl"));
+    }
+
     @Test
     public void shouldConvertCLI() throws ConverterNotFoundException, IOException {
-        String[] args = new String[] { "-in", "files", "-out", "files", "-sn", "MDL", "-sv", "5.0.8-qualm", "-tn", "NMTRAN", "-tv", "7.2.0-qualn" };
+        String[] args = new String[] { "-in", WORKING_DIRECTORY, "-out", WORKING_DIRECTORY + "output", "-sn", "MDL", "-sv", "5.0.8-qualm", "-tn", "NMTRAN", "-tv", "7.2.0-qualn" };
         Main cli = new Main();
         cli.parseArguments(args);
         ConversionReport[] reports = cli.runFromCommandLine();
         assertEquals(ConversionCode.SUCCESS, reports[0].getReturnCode());
         assertEquals(reports[0].getDetails(Severity.ERROR).size(), 0);
     }
-    
+
     @Test
-    public void shouldConvertCLIDir() throws ConverterNotFoundException, IOException {
-        String srcDirPath = Thread.currentThread().getContextClassLoader().getResource("files/").getPath();
-        String[] args = new String[] { "-in", srcDirPath, "-out", "files", "-sn", "MDL", "-sv", "5.0.8-qualm", "-tn", "NMTRAN", "-tv", "7.2.0-qualn" };
+    public void shouldConvertCLI_AbsolutePath() throws ConverterNotFoundException, IOException {
+        String[] args = new String[] { "-in", new File(WORKING_DIRECTORY).getAbsolutePath(), "-out", new File(WORKING_DIRECTORY, "output").getAbsolutePath(), "-sn", "MDL", "-sv", "5.0.8-qualm", "-tn", "NMTRAN", "-tv", "7.2.0-qualn" };
         Main cli = new Main();
         cli.parseArguments(args);
         ConversionReport[] reports = cli.runFromCommandLine();
         assertEquals(ConversionCode.SUCCESS, reports[0].getReturnCode());
         assertEquals(reports[0].getDetails(Severity.ERROR).size(), 0);
     }
-    
+
 }
