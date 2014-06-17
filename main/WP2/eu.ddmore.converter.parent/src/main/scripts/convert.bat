54257e9d5402d010d104fa8722c618a4faae4aab
Matthew Wise <mwise@mango-solutions.com>
2014-06-17 18:03:34 +0100

    DDMORE-497: Conv toolbox batch file now uses appropriate java install: The convert.bat script now uses the following hierarchy to determine the path to the java exec: 1. JRE embedded within SEE 2. JAVA_HOME environment variable 3. java on system PATH This is so that the converter-modules-aggregator assembled ZIP can be executed without requiring it to be installed within SEE.

--- main/WP2/eu.ddmore.converter.parent/src/main/scripts/convert.bat
+++ main/WP2/eu.ddmore.converter.parent/src/main/scripts/convert.bat
@@ -1,11 +1,20 @@
 @echo off
+
 SET home=%~dp0
 
 reg query "HKLM\Hardware\Description\System\CentralProcessor\0" | find /i "x86" > nul
 if %errorlevel%==0 (
-	set TRAINING_JAVA_HOME=%home%..\MDL_IDE\x86\jre
+    set TRAINING_JAVA_HOME=%home%..\MDL_IDE\x86\jre
 ) else (
-	set TRAINING_JAVA_HOME=%home%..\MDL_IDE\x86_64\jre
+    set TRAINING_JAVA_HOME=%home%..\MDL_IDE\x86_64\jre
 )
 
-%TRAINING_JAVA_HOME%\bin\java -cp "%home%/lib/*" eu.ddmore.convertertoolbox.cli.Main -in %1 -out %2 -sn %3 -sv %4 -tn %5 -tv %6
+IF EXIST {%TRAINING_JAVA_HOME%\bin\java} (
+    SET JAVA_CMD=%TRAINING_JAVA_HOME%\bin\java
+) ELSE IF EXIST {%JAVA_HOME%\bin\java} (
+    SET JAVA_CMD=%JAVA_HOME%\bin\java
+) ELSE (
+    SET JAVA_CMD=java
+)
+
+"%JAVA_CMD%" -cp "%home%/lib/*" eu.ddmore.convertertoolbox.cli.Main -in %1 -out %2 -sn %3 -sv %4 -tn %5 -tv %6
