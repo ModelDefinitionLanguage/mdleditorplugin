9dbddd23779577c5735b1d4657b27b341bda61e4
Matthew Wise <mwise@mango-solutions.com>
2014-06-20 17:36:39 +0100

    DDMORE-697: Try increasing timeout on converter execution.

--- main/WP2/eu.ddmore.converter.systemtest/src/main/java/eu/ddmore/convertertoolbox/systemtest/ConverterRunner.java
+++ main/WP2/eu.ddmore.converter.systemtest/src/main/java/eu/ddmore/convertertoolbox/systemtest/ConverterRunner.java
@@ -92,7 +92,7 @@ public class ConverterRunner {
         DefaultExecutor executor = new DefaultExecutor();
         executor.setWorkingDirectory(new File(CONVERTER_TOOLBOX_EXECUTABLE).getParentFile());
         executor.setExitValue(0); // Required "success" return code
-        ExecuteWatchdog watchdog = new ExecuteWatchdog(15000); // Will kill the process after 15 seconds
+        ExecuteWatchdog watchdog = new ExecuteWatchdog(30000); // Will kill the process after 15 seconds
         executor.setWatchdog(watchdog);
         PumpStreamHandler pumpStreamHandler = new PumpStreamHandler(stdoutOS, stderrOS);
         executor.setStreamHandler(pumpStreamHandler);
