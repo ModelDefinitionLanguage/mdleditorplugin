fe663309f1ec679cf7fd101bf66c5aed48340b09
Matthew Wise <mwise@mango-solutions.com>
2014-07-22 11:03:16 +0100

    DDMORE-726: Add a couple of methods to ConverterTestsParent superclass: - getJsonFromMDLFile(final File srcFile) - getFileFromModelsProject(final String relativePathToFile)

--- main/WP2/eu.ddmore.converter.mdl_v5.1.6_json/src/test/java/eu/ddmore/converter/mdl2json/ConverterTestsParent.groovy
+++ main/WP2/eu.ddmore.converter.mdl_v5.1.6_json/src/test/java/eu/ddmore/converter/mdl2json/ConverterTestsParent.groovy
@@ -9,35 +9,49 @@ import org.apache.log4j.Logger;
 import org.ddmore.mdl.mdl.Mcl
 
 class ConverterTestsParent {
-	final static String TEST_DATA_DIR = "./"
-	
-	private static Logger logger = Logger.getLogger(ConverterTestsParent.class)
-    
-	private static final MDLToJSONConverter converter = new MDLToJSONConverter();
 
- 	public Object getJsonFromMDLFile(String fileToConvert) {
-        File srcFile = getFile(fileToConvert)
- 
- 		MdlParser p = new MdlParser()
- 		Mcl mcl = p.parse(srcFile)
- 
+    final static String TEST_DATA_DIR = "./"
+    final static String MODELS_PROJECT_TEST_DATA_DIR = "/eu/ddmore/testdata/models/mdl/"
+    final static String WORKING_DIR = "target/MainTest_Working_Dir/"
+
+    private static Logger logger = Logger.getLogger(ConverterTestsParent.class)
+
+    private static final MDLToJSONConverter converter = new MDLToJSONConverter();
+
+    public File getFile(final String pathToFile) {
+        String path = TEST_DATA_DIR + pathToFile
+        URL url = this.getClass().getResource(path)
+        new File(url.getFile())
+    }
+
+    private File getFileFromModelsProject(final String relativePathToFile) {
+
+        final URL urlToFile = ConverterTestsParent.class.getResource(MODELS_PROJECT_TEST_DATA_DIR + relativePathToFile)
+        File destFile = new File(WORKING_DIR + relativePathToFile)
+        FileUtils.copyURLToFile(urlToFile, destFile)
+
+        return destFile
+    }
+
+    public Object getJsonFromMDLFile(final String fileToConvert) {
+        getJsonFromMDLFile(getFile(fileToConvert))
+    }
+
+    public Object getJsonFromMDLFile(final File srcFile) {
+
+        MdlParser p = new MdlParser()
+        Mcl mcl = p.parse(srcFile)
+
         String jsonText = converter.toJSON(mcl)
- 		
- 		logger.debug(jsonText)
- 		
- 		JsonSlurper slurper = new JsonSlurper();
- 		slurper.parseText(jsonText)
+
+        logger.debug(jsonText)
+
+        JsonSlurper slurper = new JsonSlurper();
+        slurper.parseText(jsonText)
     }
 
     public Object getJson(String jsonText) {
-		JsonSlurper slurper = new JsonSlurper();
-		slurper.parseText(jsonText)
+        JsonSlurper slurper = new JsonSlurper();
+        slurper.parseText(jsonText)
     }
-
-	public File getFile(final String pathToFile) {
-		String path = TEST_DATA_DIR + pathToFile
-		URL url = this.getClass().getResource(path)
-		new File(url.getFile())        
-	}
-
 }
