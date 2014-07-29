275fe224fdcf6c08fb56c1856530bcd4f5822482
Matthew Wise <mwise@mango-solutions.com>
2014-07-29 12:45:16 +0100

    DDMORE-497: Correct the applying of quotes on java paths in batch files. Specifically, fix the convert.bat batch file: the quotes need to be around the java paths in the actual set commands rather than in the command call itself.

--- main/WP2/eu.ddmore.converter.parent/src/main/scripts/convert.bat
+++ main/WP2/eu.ddmore.converter.parent/src/main/scripts/convert.bat
@@ -6,13 +6,13 @@ set TRAINING_JAVA_HOME=%SERVICE_HOME%\..\MDL_IDE\jre
 
 IF EXIST "%TRAINING_JAVA_HOME%\bin\java.exe" (
     echo Using Java from SEE
-    SET JAVA_CMD=%TRAINING_JAVA_HOME%\bin\java
+    SET JAVA_CMD="%TRAINING_JAVA_HOME%\bin\java"
 ) ELSE IF EXIST "%JAVA_HOME%\bin\java.exe" (
     echo Using Java from JAVA_HOME environment variable
-    SET JAVA_CMD=%JAVA_HOME%\bin\java
+    SET JAVA_CMD="%JAVA_HOME%\bin\java"
 ) ELSE (
     echo Falling beck to using Java from path; it will fail if Java is not installed
     SET JAVA_CMD=java
 )
 
-"%JAVA_CMD%" -cp "%home%/lib/*" eu.ddmore.convertertoolbox.cli.Main -in %1 -out %2 -sn %3 -sv %4 -tn %5 -tv %6
+%JAVA_CMD% -cp "%home%/lib/*" eu.ddmore.convertertoolbox.cli.Main -in %1 -out %2 -sn %3 -sv %4 -tn %5 -tv %6
