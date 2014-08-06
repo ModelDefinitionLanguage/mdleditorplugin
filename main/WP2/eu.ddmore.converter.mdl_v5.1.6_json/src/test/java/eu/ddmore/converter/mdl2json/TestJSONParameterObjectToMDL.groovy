4de27213da4fe0766161bedbf03e5273785719de
Matthew Wise <mwise@mango-solutions.com>
2014-08-06 17:25:54 +0100

    DDMORE-735: Unit test for Parameter::Variability JSON->MDL conv.

--- main/WP2/eu.ddmore.converter.mdl_v5.1.6_json/src/test/java/eu/ddmore/converter/mdl2json/TestJSONParameterObjectToMDL.groovy
+++ main/WP2/eu.ddmore.converter.mdl_v5.1.6_json/src/test/java/eu/ddmore/converter/mdl2json/TestJSONParameterObjectToMDL.groovy
@@ -1,16 +1,14 @@
 package eu.ddmore.converter.mdl2json;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.*
 
+import org.apache.commons.io.FileUtils;
 import org.apache.log4j.Logger
+import org.junit.Test
 
 import eu.ddmore.converter.mdl2json.domain.MCLFile
 import eu.ddmore.converter.mdl2json.domain.Parameter
-import eu.ddmore.convertertoolbox.api.response.ConversionReport
-import eu.ddmore.mdlparse.MdlParser
 import groovy.json.JsonSlurper
-import org.ddmore.mdl.mdl.Mcl
-import org.junit.Test;
 
 class TestJSONParameterObjectToMDL extends ConverterTestsParent  {
 	private static Logger logger = Logger.getLogger(TestJSONParameterObjectToMDL.class)
@@ -51,37 +49,41 @@ class TestJSONParameterObjectToMDL extends ConverterTestsParent  {
 		assertEquals(expected, p.toMDL())
 	}
 	
-	@Test	
-	public void testSimpleVariabilityModel() {
-		def simpleVariabilityModel="""{\"VARIABILITY\":[
-		{\"matrix\":{\"name\":\"struc1",\"type\":\"VAR\",\"content\":\"PPV_PRL0=.425,\n.758,PPV_KI=1.82\"}},
-		{\"PPV_PHS2\":{\"value\":\"1.80\",\"type\":\"VAR\"}},
-		{\"diag\":{\"name\":\"struc2\",\"type\":\"VAR\",\"content\":\"PPV_IOV_IN_PRL0_1=.0657\"}},
-		{\"same\":{\"name\":\"struc2\",\"content\":[\"PPV_IOV_IN_PRL0_2\"]}},
-		{\"RUV_EPS1\":{\"value\":\"1\",\"type\":\"VAR\",\"fix\":\"true\"}}]
-}"""
-
-		def json = new JsonSlurper().parseText(simpleVariabilityModel)
+	@Test
+	public void testVariabilityModel() {
+		final URL url = this.getClass().getResource("/eu/ddmore/converter/mdl2json/parameterVariabilityModel.json")
 		
-		Parameter p = new Parameter(json)
+		def json = FileUtils.readFileToString(new File(url.getFile()))
+		Parameter param = new Parameter(new JsonSlurper().parseText(json))
+		
+		logger.debug(param.toMDL())
 		
 		String expected = """parobj {
     VARIABILITY{
-        matrix(name="struc1", type="VAR"){
+        matrix(name="struc1", type=VAR) {
             PPV_PRL0=.425,
             .758,PPV_KI=1.82
         }
+        PPV_KOUT=list(value=0.589,type=VAR)
+        PPV_AMP1=list(value=.0558,type=VAR)
         PPV_PHS2=list(value=1.80,type=VAR)
-        diag(name="struc2", type="VAR"){
+        diag(name="struc2", type=VAR) {
             PPV_IOV_IN_PRL0_1=.0657
         }
-        same(name="struc2"){
+        same(name="struc2") {
             PPV_IOV_IN_PRL0_2
+        }
+        same(name="struc2") {
+            PPV_IOV_IN_PRL0_3
+        }
+        same(name="struc2") {
+            PPV_IOV_IN_PRL0_4
         }
         RUV_EPS1=list(fix=true,value=1,type=VAR)
     }
 }
 """
-		assertEquals(expected, p.toMDL())
+		assertEquals(expected, param.toMDL())
 	}
+	
 }
