9edabcd7f15b9ca2772386dc83a3ad467f1bf369
Matthew Wise <mwise@mango-solutions.com>
2014-08-12 12:19:01 +0100

    DDMORE-722: Update JSON->MDL conv test operating on R generated MOG.

--- main/WP2/eu.ddmore.converter.mdl_v5.1.6_json/src/test/java/eu/ddmore/converter/mdl2json/TestJSONToMDLConverter.groovy
+++ main/WP2/eu.ddmore.converter.mdl_v5.1.6_json/src/test/java/eu/ddmore/converter/mdl2json/TestJSONToMDLConverter.groovy
@@ -23,15 +23,23 @@ class TestJSONToMDLConverter extends ConverterTestsParent {
 
 		MCLFile mclFile = new MCLFile(json)
 		
-		assertNotNull(mclFile.toMDL() )			
+		assertNotNull(mclFile.toMDL())
 
 	}
 
+	/**
+	 * The ex_model7_prolactin_25June2014_OAM.mdl MDL file was run through the MDL->JSON->R->JSON->MDL pipeline.
+	 * The JSON on the way out was dumped out and saved as the "prolactinOutputMog.json" file.
+	 * This test converts this JSON into MDL and compares certain blocks of this against the corresponding blocks
+	 * taken from the original Prolactin MDL.
+	 * If any of the parts of the aforementioned conversion pipeline are modified then the updated JSON should be
+	 * re-captured and re-saved.
+	 * Ultimately this would be an automated integration test... DDMORE-845.
+	 */
     @Test
-	@Ignore("Pending re-generation of MOG from R")
 	public void testRGeneratedMOG() {
 		
-		File jsonFile = getFile("myMog3.json")
+		File jsonFile = getFile("prolactinOutputMog.json")
 		
 		String content = jsonFile.getText()
 		
@@ -41,9 +49,42 @@ class TestJSONToMDLConverter extends ConverterTestsParent {
 		
 		String mdl = mclFile.toMDL()
 		
-		assertNotEquals(null, mdl )			
-		
+		assertNotNull(mdl)
+	
 		logger.debug(mdl)
+		
+		def dataInputVars = TestJSONModelToMDL.extractSpecificBlock(mdl, "DATA_INPUT_VARIABLES")
+			
+		assertEquals("Checking content of DATA_INPUT_VARIABLES block",
+"""    DATA_INPUT_VARIABLES {
+        STU=list(type=continuous)
+        ID=list(type=categorical)
+        TRT=list(type=continuous)
+        NPER=list(type=continuous)
+        TIME=list(type=continuous,units="h")
+        CMT=list(type=categorical)
+        AMT=list(type=continuous,units="mg")
+        RATE=list(type=continuous)
+        EVID=list(type=categorical)
+        SEX=list(type=categorical)
+        PRL=list(type=continuous)
+        DV=list(type=continuous)
+        CLR=list(type=continuous)
+        VC=list(type=continuous)
+        VP=list(type=continuous)
+        QB=list(type=continuous)
+        KS=list(type=continuous)
+        FR=list(type=continuous)
+        DUR=list(type=continuous)
+        ALAG=list(type=continuous)
+        PAT=list(type=continuous)
+        SS=list(type=categorical)
+        II=list(type=continuous,units="h")
+    }
+""", dataInputVars)
+		
+		// TODO: Ideally we'd have similar tests for all the blocks
+		
 	}
 	
 }
