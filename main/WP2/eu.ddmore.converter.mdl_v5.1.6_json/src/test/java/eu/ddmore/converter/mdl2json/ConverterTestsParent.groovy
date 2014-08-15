430fe1ea377f6da32a484ffecd901166b3d0c2e2
mwise <mwise@matt-wise.Mango.local>
2014-08-15 17:41:01 +0100

    DDMORE-726: Implement semi-automated conversion tests of MDL->JSON->MDL: Specifically, for each of MDL models Prolactin, Tumour, DrugX, Warfarin, import the MDL into R via JSON and write it straight back out, dumping out the output JSON on the way out and saving as .output.json files within the testdata models project. Then in TestJSONToMDLConverter, verify each block of each of these resulting MDL files against the corresponding block of the corresponding original MDL. However, the actual tests are @Ignored for the time being pending fixes to the R conversion code.

--- main/WP2/eu.ddmore.converter.mdl_v5.1.6_json/src/test/java/eu/ddmore/converter/mdl2json/ConverterTestsParent.groovy
+++ main/WP2/eu.ddmore.converter.mdl_v5.1.6_json/src/test/java/eu/ddmore/converter/mdl2json/ConverterTestsParent.groovy
@@ -14,7 +14,7 @@ import java.util.regex.Matcher
 class ConverterTestsParent {
 
     final static String TEST_DATA_DIR = "./"
-    final static String MODELS_PROJECT_TEST_DATA_DIR = "/eu/ddmore/testdata/models/mdl/"
+    final static String MODELS_PROJECT_TEST_DATA_DIR = "/eu/ddmore/testdata/models/"
     final static String WORKING_DIR = "target/MainTest_Working_Dir/"
 
     private static Logger logger = Logger.getLogger(ConverterTestsParent.class)
@@ -27,14 +27,36 @@ class ConverterTestsParent {
         new File(url.getFile())
     }
 
+	/**
+	 * Return a MDL {@link File} from the testdata models project.
+	 * <p>
+	 * @param relativePathToFile - the relative path to the MDL file within the directory /eu/ddmore/testdata/models/mdl/
+	 * 							   within the testdata models project
+	 * @return the MDL {@link File}
+	 * @throws <code>NullPointerException</code> if the referenced file does not exist
+	 * @see #getFileFromModelsProject(String, String)
+	 */
     public File getFileFromModelsProject(final String relativePathToFile) {
-
-        final URL urlToFile = ConverterTestsParent.class.getResource(MODELS_PROJECT_TEST_DATA_DIR + relativePathToFile)
-        File destFile = new File(WORKING_DIR + relativePathToFile)
-        FileUtils.copyURLToFile(urlToFile, destFile)
-
-        return destFile
+		getFileFromModelsProject(relativePathToFile, "mdl")
     }
+	
+	/**
+	 * Return a model {@link File} from the testdata models project.
+	 * <p>
+	 * @param relativePathToFile - the relative path to the file within the directory /eu/ddmore/testdata/models/[modelType]/
+	 * 							   within the testdata models project
+	 * @param modelType - the model-type subdirectory (e.g. "mdl", "ctl")
+	 * @return the {@link File}
+	 * @throws <code>NullPointerException</code> if the referenced file does not exist
+	 */
+	public File getFileFromModelsProject(final String relativePathToFile, final String modelType) {
+		
+		final URL urlToFile = ConverterTestsParent.class.getResource(MODELS_PROJECT_TEST_DATA_DIR + modelType + "/" + relativePathToFile)
+		File destFile = new File(WORKING_DIR + relativePathToFile)
+		FileUtils.copyURLToFile(urlToFile, destFile)
+		
+		return destFile
+	}
 
     public Object getJsonFromMDLFile(final String fileToConvert) {
         getJsonFromMDLFile(getFile(fileToConvert))
@@ -68,13 +90,16 @@ class ConverterTestsParent {
 		def String origMdlFileBlockContent = putParameterListsIntoKnownOrder(extractSpecificBlock(origMdlFileContent, blockName))
 		def String newMdlFileBlockContent = putParameterListsIntoKnownOrder(extractSpecificBlock(newMdlFileContent, blockName))
 		
-		// Trim off whitespace from both the expected and the actual
-		// Also drop any { } brackets around an "if" statement which are always added when writing out to MDL
-		// TODO: Better way of dealing with this, since incorrect bracketing wouldn't be picked up as things stand
-		assertEquals("Checking the content of the block " + blockName,
-			origMdlFileBlockContent.replaceAll(~/\s*/, "").replaceAll(~/if\((.+?)\)\{(.*?)\}/, /if($1)$2/),
-			newMdlFileBlockContent.replaceAll(~/\s*/, "").replaceAll(~/if\((.+?)\)\{(.*?)\}/, /if($1)$2/))
+		if (!StringUtils.isEmpty(origMdlFileBlockContent) || !StringUtils.isEmpty(newMdlFileBlockContent)) { // Check that we actually have something to compare
+			logger.info("Verifying block " + blockName + "...")
 		
+			// Trim off whitespace from both the expected and the actual
+			// Also drop any { } brackets around an "if" statement which are always added when writing out to MDL
+			// TODO: Better way of dealing with this, since incorrect bracketing wouldn't be picked up as things stand
+			assertEquals("Checking the content of the block " + blockName,
+				origMdlFileBlockContent.replaceAll(~/\s*/, "").replaceAll(~/if\((.+?)\)\{(.*?)\}/, /if($1)$2/),
+				newMdlFileBlockContent.replaceAll(~/\s*/, "").replaceAll(~/if\((.+?)\)\{(.*?)\}/, /if($1)$2/))
+		}
 	}
 	
 	private static String readInAndStripComments(final File origMdlFile) {
@@ -94,10 +119,12 @@ class ConverterTestsParent {
 	private static String extractSpecificBlock(final String mdlFileContent, final String blockName) {
 		final StringBuffer strBuf = new StringBuffer()
 		
+		def found = false;
 		def interestedIn = false
 		def nestingLevel = -1
 		mdlFileContent.eachLine { str ->
 			if (str.matches("^\\s*" + blockName + "\\s*\\{")) {
+				found = true
 				interestedIn = true
 				// There might be open curly brackets before the name of the block name; the '-' sign on the RHS is deliberate
 				nestingLevel = - StringUtils.countMatches(str.split(blockName)[0], "{")
@@ -117,6 +144,10 @@ class ConverterTestsParent {
 			}
 		}
 		
+		if (!found) {
+			logger.info("Block \"" + blockName + "\" was not found in the MDL")
+		}
+		
 		strBuf.toString()
 	}
 	
@@ -131,7 +162,16 @@ class ConverterTestsParent {
 	 */
 	private static String putParameterListsIntoKnownOrder(final String str) {
 		
-		final Matcher matcher = ( str =~ /(?:=list|\~)\s*\(\s*(.+)\s*\)/)
+		// Some explanation of this regex needed!
+		// The (?s) is the "dot-all" instruction to the matcher to match newline characters,
+		// so "list(...)"s spread over multiple lines will match.
+		// "(?:=list|\~)" is for matching either "=list(...)" or "~ (...)"; the "?:" instructs
+		// the matcher to not actually capture this group.
+		// The remainder of the regex is to match the parameter list in the ( ) brackets; the
+		// "?" after the ".+" instructs the matcher to lazily match rather than greedily match,
+		// otherwise the matcher would keep going until it found the last closing bracket rather
+		// than the one that matched the opening bracket.
+		final Matcher matcher = ( str =~ /(?s)(?:=list|\~)\s*\(\s*(.+?)\s*\)/)
 		
 		def outStr = str
 		
@@ -139,8 +179,6 @@ class ConverterTestsParent {
 			String[] params = matcher.group(1).split(/\s*,\s*/)
 			outStr = outStr.replace(matcher.group(1), params.sort().join(","))
 		}
-		
-		logger.debug("outStr = " + outStr)
 		
 		outStr
 	}
--- main/WP2/eu.ddmore.converter.mdl_v5.1.6_json/src/test/java/eu/ddmore/converter/mdl2json/TestJSONToMDLConverter.groovy
+++ main/WP2/eu.ddmore.converter.mdl_v5.1.6_json/src/test/java/eu/ddmore/converter/mdl2json/TestJSONToMDLConverter.groovy
@@ -8,101 +8,124 @@ import org.junit.Ignore
 
 import eu.ddmore.converter.mdl2json.domain.MCLFile
 
+/**
+ * "Real" MDL files from the testdata models project were run through the
+ * MDL->JSON->R->JSON->MDL pipeline.
+ * <p>
+ * The JSON on the way out was dumped out and saved as corresponding .output.json files,
+ * again into the testdata models project.
+ * <p>
+ * These tests convert these JSONs into MDL and compare each block of these against the corresponding
+ * block taken from the corresponding original MDL.
+ * <p>
+ * <b>If any of the parts of the aforementioned conversion pipeline are modified then the updated JSONs
+ * should be re-captured and re-saved.</b>
+ * <p>
+ * Ultimately this would be an automated integration test... DDMORE-845.
+ */
 class TestJSONToMDLConverter extends ConverterTestsParent {
 	private static Logger logger = Logger.getLogger(TestJSONToMDLConverter.class)
-	
-	@Test
-	public void testProlactinMay2014() {
-        
-        def File mdlFile = getFileFromModelsProject("FribergCPT2009/ex_model7_prolactin_01July2014_OAM.mdl")
-        def json = getJsonFromMDLFile(mdlFile)
-
-		MCLFile mclFile = new MCLFile(json)
-		
-		assertNotNull(mclFile.toMDL())
-
-	}
 
 	/**
-	 * The ex_model7_prolactin_25June2014_OAM.mdl MDL file was run through the MDL->JSON->R->JSON->MDL pipeline.
-	 * The JSON on the way out was dumped out and saved as the "prolactinOutputMog.json" file.
-	 * This test converts this JSON into MDL and compares certain blocks of this against the corresponding blocks
-	 * taken from the original Prolactin MDL.
-	 * If any of the parts of the aforementioned conversion pipeline are modified then the updated JSON should be
-	 * re-captured and re-saved.
-	 * Ultimately this would be an automated integration test... DDMORE-845.
+	 * Original MDL file: ex_model7_prolactin_01July2014_OAM.mdl
+	 * File containing the dumped out JSON: ex_model7_prolactin_01July2014_OAM.output.json
 	 */
     @Test
-	@Ignore("Pending regeneration of JSON from R")
-	public void testRGeneratedMOG() {
+	// Empty blocks:
+	@Ignore("Absence of ESTIMATION block (and possibly others) produces a map entry mapping to null in the output JSON which gives rise to an empty block in the MDL - this needs to be fixed")
+	public void testRGeneratedMOG_Prolactin() {
+		testRGeneratedMOG(
+			"FribergCPT2009/ex_model7_prolactin_01July2014_OAM.output.json",		
+			"FribergCPT2009/ex_model7_prolactin_01July2014_OAM.mdl"
+		);
+	}
+	
+	/**
+	 * Original MDL file: tumour_size_01July2014_OAM.mdl
+	 * File containing the dumped out JSON: tumour_size_01July2014_OAM.output.json
+	 */
+	@Test
+	@Ignore("Absence of Ignore character gives \"ignore=[]\" in R->JSON conversion - this needs to be fixed")
+	public void testRGeneratedMOG_Tumour() {
+		testRGeneratedMOG(
+			"ThamCCR2008/tumour_size_01July2014_OAM.output.json",
+			"ThamCCR2008/tumour_size_01July2014_OAM.mdl"
+		);
+	}
+	
+	/**
+	 * Original MDL file: drugX_trAbs_1Rep_EST001_ORG.mdl
+	 * File containing the dumped out JSON: drugX_trAbs_1Rep_EST001_ORG.output.json
+	 */
+	@Test
+	@Ignore("Absence of RANDOM_VARIABLE_DEFINITION block (and possibly others) produces an empty list in the output JSON which gives rise to an empty block in the MDL - this needs to be fixed")
+	// Problematic empty blocks:-
+	// RANDOM_VARIABLE_DEFINITION
+	// LIBRARY
+	// OBSERVATION
+	// ESTIMATION
+	public void testRGeneratedMOG_DrugX() {
+		testRGeneratedMOG(
+			"drugX/drugX_trAbs_1Rep_EST001_ORG.output.json",
+			"drugX/drugX_trAbs_1Rep_EST001_ORG.mdl"
+		);
+	}
+	
+	/**
+	 * Original MDL file: warfarin_PK_PRED.mdl
+	 * File containing the dumped out JSON: warfarin_PK_PRED.output.json
+	 */
+	@Test
+	@Ignore("Ignore char being hash character # -> everything after this treated as a comment so stripped off (in the verification code in the test) - this needs to be fixed")
+	// Problematic empty blocks:-
+	// MODEL_PREDICTION
+	// ODE
+	// LIBRARY
+	// ESTIMATION
+	public void testRGeneratedMOG_Warfarin() {
+		testRGeneratedMOG(
+			"warfarin_PK_PRED/warfarin_PK_PRED.output.json",
+			"warfarin_PK_PRED/warfarin_PK_PRED.mdl"
+		);
+	}
+	
+	
+	private void testRGeneratedMOG(final String pathToOutputJsonFile, final String pathToOrigMdlFile) {
 		
-		File jsonFile = getFile("prolactinOutputMog.json")
+		def File jsonFile = getFileFromModelsProject(pathToOutputJsonFile, "json")
 		
-		String content = jsonFile.getText()
+		String rawJson = jsonFile.getText()
+				
+		def json = getJson(rawJson)
 		
-		def json = getJson(content)
-
 		MCLFile mclFile = new MCLFile(json)
 		
 		String mdl = mclFile.toMDL()
-		
-		assertNotNull(mdl)
 	
 		logger.debug(mdl)
 		
-		def dataInputVars = TestJSONModelToMDL.extractSpecificBlock(mdl, "DATA_INPUT_VARIABLES")
-		assertEquals("Checking content of DATA_INPUT_VARIABLES block",
-"""    DATA_INPUT_VARIABLES {
-        STU=list(type=continuous)
-        ID=list(type=categorical)
-        TRT=list(type=continuous)
-        NPER=list(type=continuous)
-        TIME=list(type=continuous,units="h")
-        CMT=list(type=categorical)
-        AMT=list(type=continuous,units="mg")
-        RATE=list(type=continuous)
-        EVID=list(type=categorical)
-        SEX=list(type=categorical)
-        PRL=list(type=continuous)
-        DV=list(type=continuous)
-        CLR=list(type=continuous)
-        VC=list(type=continuous)
-        VP=list(type=continuous)
-        QB=list(type=continuous)
-        KS=list(type=continuous)
-        FR=list(type=continuous)
-        DUR=list(type=continuous)
-        ALAG=list(type=continuous)
-        PAT=list(type=continuous)
-        SS=list(type=categorical)
-        II=list(type=continuous,units="h")
-    }
-""", dataInputVars)
-
-		def structuralParams = TestJSONModelToMDL.extractSpecificBlock(mdl, "STRUCTURAL")
-		assertEquals("Checking content of STRUCTURAL block",
-"""    STRUCTURAL {
-        POP_KOUT=list(lo=.1,value=0.664,hi=10)
-        POP_PRL0_IN_MALE_HV=list(lo=1,value=7.67,hi=15)
-        POP_PRL0_IN_MALE_PAT=list(lo=1,value=16.1,hi=30)
-        POP_PRL0_IN_FEMALES_PAT=list(lo=1,value=35.2,hi=40)
-        POP_PRL0_IN_MALES_STUDY_101=list(lo=0,value=11.3)
-        POP_PRL0_IN_FEMALES_STUDY_101=list(lo=0,value=23.2)
-        POP_KDA=list(lo=0.001,value=.156,hi=50)
-        POP_UPDA=list(lo=0.001,value=1.44,hi=10)
-        POP_AMP=list(lo=-.75,value=0.532,hi=1.5)
-        POP_PHS1=list(lo=12,value=20.1,hi=25)
-        POP_AMP2=list(lo=-.9,value=-.314,hi=0.2)
-        POP_PHS2=list(lo=-2,value=13.7,hi=15)
-        POP_KI=list(lo=0,value=1.96,hi=100)
-        POP_PHASE_SHIFT_IN_PATIENTS=list(lo=-5,value=-1.61)
-        POP_RES_ERR_IN_MALE_HV=list(lo=0,value=.290)
-        POP_RES_ERR_IN_MALE_PATIENTS=list(lo=0,value=.422)
-        POP_RES_ERR_IN_FEMALE_PATIENTS=list(lo=0,value=.571)
-    }
-""", structuralParams)
+		def File origMdlFile = getFileFromModelsProject(pathToOrigMdlFile, "mdl")
 		
-		// TODO: Ideally we'd have similar tests for all the blocks
+		[
+			"DATA_INPUT_VARIABLES",
+			"SOURCE",
+			"STRUCTURAL",
+			"VARIABILITY",
+			"MODEL_INPUT_VARIABLES",
+			"STRUCTURAL_PARAMETERS",
+			"VARIABILITY_PARAMETERS",
+			"GROUP_VARIABLES",
+			"RANDOM_VARIABLE_DEFINITION",
+			"INDIVIDUAL_VARIABLES",
+			"MODEL_PREDICTION",
+			"ODE", // actually redundant, should be verified by MODEL_PREDICTION block
+			"LIBRARY", // actually redundant, should be verified by MODEL_PREDICTION block
+			"OBSERVATION",
+			"ESTIMATION",
+			"MODEL_OUTPUT_VARIABLES",
+		].each { blockName ->
+			extractBlockFromOriginalMDLAndCompareIgnoringWhitespaceAndComments(origMdlFile, blockName, mdl)
+		}
 		
 	}
 	
