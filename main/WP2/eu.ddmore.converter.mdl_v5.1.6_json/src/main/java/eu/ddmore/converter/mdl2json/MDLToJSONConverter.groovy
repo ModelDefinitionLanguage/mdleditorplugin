41dc1e889ba642bb3d7b95dc46004a48dad3e38c
Matthew Wise <mwise@mango-solutions.com>
2014-12-08 10:14:32 +0000

    DDMORE-908: MDL v 5.1.6 is now v 5.1.6-interop - amend MDL<->JSON conv

--- main/WP2/eu.ddmore.converter.mdl_v5.1.6_json/src/main/java/eu/ddmore/converter/mdl2json/MDLToJSONConverter.groovy
+++ main/WP2/eu.ddmore.converter.mdl_v5.1.6_json/src/main/java/eu/ddmore/converter/mdl2json/MDLToJSONConverter.groovy
@@ -35,7 +35,7 @@ public class MDLToJSONConverter implements ConverterProvider {
 	private static final String MDL_FILE_EXTENSION = ".mdl"
 	private static final String JSON_FILE_EXTENSION = ".json"	
 	
-	private final LanguageVersion source = new LanguageVersionImpl("MDL", new VersionImpl(5, 1, 6))
+	private final LanguageVersion source = new LanguageVersionImpl("MDL", new VersionImpl(5, 1, 6, "interop"))
     private final LanguageVersion target = new LanguageVersionImpl("JSON", new VersionImpl(0, 0, 0))
     private final Version converterVersion = new VersionImpl(1, 0, 0);
 
