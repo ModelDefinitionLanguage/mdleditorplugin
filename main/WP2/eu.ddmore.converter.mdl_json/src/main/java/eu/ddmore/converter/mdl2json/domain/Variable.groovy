6df606fc74ef43ac176a99d7f9a5412a1776579c
Matthew Wise <mwise@mango-solutions.com>
2015-02-04 17:10:23 +0000

    DDMORE-1044: Merge randomVarAttrs stuff from RandomVar into Variable. This is in preparation for RandomVariable being removed and replaced by this more generic Variable.

--- main/WP2/eu.ddmore.converter.mdl_json/src/main/java/eu/ddmore/converter/mdl2json/domain/Variable.groovy
+++ main/WP2/eu.ddmore.converter.mdl_json/src/main/java/eu/ddmore/converter/mdl2json/domain/Variable.groovy
@@ -18,19 +18,30 @@ public class Variable extends Expando implements MDLPrintable {
     
     public static final String NAME_KEY = ".name"
     public static final String EXPRESSION_KEY = ".expr"
+    public static final String RANDOMVAR_ATTRS_KEY = ".random_var_attrs"
 	
 	public Variable(final org.ddmore.mdl.mdl.SymbolDeclaration sd) {
 		
 		setProperty(NAME_KEY, sd.getSymbolName().getName())
 	
 		if (sd.getList()) {
+            // 'Normal' attributes, add them to the Map of properties directly
 			sd.getList().getArguments().getArguments().each {Argument a ->
 				setProperty(a.getArgumentName().getName(), XtextWrapper.unwrap(a.getExpression()))
 			}
 		}
+        else if (sd.getRandomList()) {
+            // Create a nested Map of these attributes, to distinguish them from 'normal' attributes
+            final Map randomVarAttrs = [:]
+            sd.getRandomList().getArguments().getArguments().each {Argument a ->
+                randomVarAttrs.put(a.getArgumentName().getName(), XtextWrapper.unwrap(a.getExpression()))
+            }
+            setProperty(RANDOMVAR_ATTRS_KEY, randomVarAttrs)
+        }
         else if (sd.getExpression() != null) {
             setProperty(EXPRESSION_KEY, XtextWrapper.unwrap(sd.getExpression()))
         }
+        
 	}
 	
 	public Variable(final SymbolName sn) {
@@ -48,22 +59,38 @@ public class Variable extends Expando implements MDLPrintable {
     public String getExpression() {
         return (String) getProperty(EXPRESSION_KEY)
     }
+    
+    public Map getRandomVarAttributes() {
+        return (Map) getProperty(RANDOMVAR_ATTRS_KEY)
+    }
 	
 	public String toMDL() {
-	    // NB: NAME_KEY and EXPRESSION_KEY need to be in parentheses to be treated as string variables rather than string literals
-        final Map specialAttributes = [(NAME_KEY):getName(), (EXPRESSION_KEY):getExpression()]
+	    // NB: NAME_KEY, EXPRESSION_KEY, RANDOMVAR_ATTRS_KEY need to be in parentheses
+        // in order to be treated as string variables rather than string literals
+        final Map specialAttributes = [(NAME_KEY):getName(), (EXPRESSION_KEY):getExpression(), (RANDOMVAR_ATTRS_KEY):getRandomVarAttributes()]
         
 		List attributes = []
 		// Note: sorting is only done so that we get predictable MDL strings that we can compare in the tests
 		getProperties().minus(specialAttributes).sort().each { k, v ->
 			attributes.add("${k}=${v}")
 		}
+        
         if (getExpression()) {
             """${getName()} = ${getExpression()}"""
-        } else if (attributes.isEmpty()) {
-			"""${getName()}"""
-		} else {
-			"""${getName()} : {${attributes.join(", ")}}"""
+        }
+        else if (getRandomVarAttributes()?.isEmpty()) {
+            Map randomVarAttrs = [:]
+            // Note: sorting is only done so that we get predictable MDL strings that we can compare in the tests
+            getRandomVarAttributes().sort().each { k, v ->
+                randomVarAttrs.add("${k}=${v}")
+            }
+            """${getName()} ~ {${randomVarAttrs.join(", ")}}"""
+        }
+        else if (!attributes.isEmpty()) {
+            """${getName()} : {${attributes.join(", ")}}"""
+		}
+        else {
+            """${getName()}"""
 		}
 	}
 	
