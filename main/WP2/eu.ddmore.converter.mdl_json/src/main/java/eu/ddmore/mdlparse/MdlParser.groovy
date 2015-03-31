cbbe01656ae3a7e43da490c263274e412ae677b0
Matthew Wise <mwise@mango-solutions.com>
2015-03-31 17:57:46 +0100

    DDMORE-1158: MdlParser.parse() now throws excptn if unable to parse MDL

--- main/WP2/eu.ddmore.converter.mdl_json/src/main/java/eu/ddmore/mdlparse/MdlParser.groovy
+++ main/WP2/eu.ddmore.converter.mdl_json/src/main/java/eu/ddmore/mdlparse/MdlParser.groovy
@@ -1,17 +1,20 @@
 package eu.ddmore.mdlparse
 
-import java.io.File;
+import org.apache.log4j.Logger
+import org.ddmore.mdl.MdlStandaloneSetup
+import org.ddmore.mdl.mdl.Mcl
+import org.eclipse.emf.common.util.EList
+import org.eclipse.emf.common.util.URI
+import org.eclipse.emf.ecore.resource.Resource
+import org.eclipse.emf.ecore.resource.Resource.Diagnostic
+import org.eclipse.xtext.parser.ParseException
+import org.eclipse.xtext.resource.XtextResource
+import org.eclipse.xtext.resource.XtextResourceSet
 
-import org.ddmore.mdl.MdlStandaloneSetup;
-import org.ddmore.mdl.mdl.Mcl;
-import org.eclipse.emf.common.util.URI;
-import org.eclipse.emf.ecore.resource.Resource;
-import org.eclipse.xtext.resource.XtextResource;
-import org.eclipse.xtext.resource.XtextResourceSet;
-
-import com.google.inject.Injector;
+import com.google.inject.Injector
 
 class MdlParser {
+    private final static Logger LOGGER = Logger.getLogger(MdlParser.class)
 
 	static Injector injector;
 	static XtextResourceSet resourceSet;
@@ -28,7 +31,24 @@ class MdlParser {
 	}
 	
 	public Mcl parse(File mdlFile) {
-        Resource resource = resourceSet.getResource(URI.createURI("file:///" + mdlFile.getAbsolutePath()), true);
+        final Resource resource = resourceSet.getResource(URI.createURI("file:///" + mdlFile.getAbsolutePath()), true)
+        
+        EList<Diagnostic> errors = resource.getErrors()
+        EList<Diagnostic> warnings = resource.getWarnings()
+        if (errors) {
+            LOGGER.error(errors.size() + " errors encountered in parsing MDL file " + mdlFile.getAbsolutePath())
+            for (Diagnostic e : errors) {
+                LOGGER.error(e)
+            }
+            throw new ParseException("Unable to parse MDL file " + mdlFile.getAbsolutePath() + "; " + errors.size() + " error(s) encountered; see the log output.")
+        }
+        if (warnings) {
+            LOGGER.error(warnings.size() + " warning(s) encountered in parsing MDL file " + mdlFile.getAbsolutePath())
+            for (Diagnostic w : warnings) {
+                LOGGER.error(w)
+            }
+        }
+        
 		return (Mcl) resource.getContents().get(0);
 	}
 
