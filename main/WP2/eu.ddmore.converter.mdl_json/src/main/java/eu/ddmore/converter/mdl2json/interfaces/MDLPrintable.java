dd78f63f2915b86878a726d3d52093dc3325a929
Matthew Wise <mwise@mango-solutions.com>
2015-04-23 16:01:12 +0100

    DDMORE-1158: JavaDoc only, no functional changes

--- main/WP2/eu.ddmore.converter.mdl_json/src/main/java/eu/ddmore/converter/mdl2json/interfaces/MDLPrintable.java
+++ main/WP2/eu.ddmore.converter.mdl_json/src/main/java/eu/ddmore/converter/mdl2json/interfaces/MDLPrintable.java
@@ -4,6 +4,9 @@ public interface MDLPrintable {
 	
 	public static final String IDT = "    ";
 
+	/**
+	 * @return the MDL textual representation of this object, i.e. as per the original MDL file
+	 */
 	public String toMDL();
 
 }
