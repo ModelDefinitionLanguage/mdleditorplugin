a9d76ffcf3a3280a7b35a2953c9b1eaf9257f806
Matthew Wise <mwise@mango-solutions.com>
2015-05-29 09:33:36 +0100

    DDMORE-1158: Provide full year in copyright date ranges

--- main/WP2/eu.ddmore.converter.mdl_json/src/main/java/eu/ddmore/converter/mdl2json/domain/Parameter.groovy
+++ main/WP2/eu.ddmore.converter.mdl_json/src/main/java/eu/ddmore/converter/mdl2json/domain/Parameter.groovy
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (C) 2014-5 Mango Solutions Ltd - All rights reserved.
+ * Copyright (C) 2014-2015 Mango Solutions Ltd - All rights reserved.
  ******************************************************************************/
 package eu.ddmore.converter.mdl2json.domain;
 
